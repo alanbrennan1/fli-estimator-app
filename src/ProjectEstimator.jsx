9dcc0b9119744b02cc526b06ccb96884c0d452af
alanbrennan1 <alanbrennan1@gmail.com>
2025-04-29 16:31:15 +0100

    Update ProjectEstimator.jsx

--- src/ProjectEstimator.jsx
+++ src/ProjectEstimator.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
+import pricingMap from './pricing.json';
 
 // State for pricing data
 const [pricingData, setPricingData] = useState([]);
