14f55221b36e7762370486e48a530637b99770ea
alanbrennan1 <alanbrennan1@gmail.com>
2025-04-29 15:49:53 +0100

    Update ProjectEstimator.jsx

--- src/ProjectEstimator.jsx
+++ src/ProjectEstimator.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './index.css';
 
 function AccordionSection({ title, children }) {
