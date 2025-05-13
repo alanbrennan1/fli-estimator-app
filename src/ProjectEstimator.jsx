d628a83d70748fbf05dcefb054ed8e3aba13c826
alanbrennan1 <alanbrennan1@gmail.com>
2025-05-13 16:36:35 +0100

    Update ProjectEstimator.jsx

--- src/ProjectEstimator.jsx
+++ src/ProjectEstimator.jsx
@@ -363,7 +363,8 @@ if (inputs.antiFlotation === 'Yes') {
 
         concreteVolume = (chamberVol + antiVol) * quantity;
 
-        inputs.antiFlotationVolume = antiVol;
+        inputs.antiFlotationVolume = antiVol * quantity;
+
       }
 
       const steelKg = concreteVolume * 120;
@@ -389,8 +390,7 @@ if (inputs.antiFlotation === 'Yes') {
         quantity,
 concrete: {
   volume: concreteVolume.toFixed(2),
-  antiVol: (inputs.antiFlotation === 'Yes' && antiVol > 0) ? antiVol.toFixed(2) : undefined
-
+  antiVol: (inputs.antiFlotation === 'Yes' && antiVol > 0) ? (antiVol * quantity).toFixed(2) : undefined
 
 },
        
