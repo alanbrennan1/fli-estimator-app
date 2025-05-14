6e81ddcebea995c47980ce8a3f21c4fb0b08344f
alanbrennan1 <alanbrennan1@gmail.com>
2025-05-14 13:15:01 +0100

    Update ProjectEstimator.jsx

--- src/ProjectEstimator.jsx
+++ src/ProjectEstimator.jsx
@@ -978,7 +978,7 @@ const handleChange = (e) => {
 
 <div className="mt-4"></div>
     {/* Inputs - Unique */}
-        <div className="mt-10 mb-6 border-2 border-gray-300 shadow-md rounded-lg p-4 bg-white">
+        <div className="mt-4 mb-6 border-2 border-gray-300 shadow-md rounded-lg p-4 bg-white">
           <h4 className="text-sm font-bold uppercase text-gray-700 mb-4 tracking-wide border-b border-gray-400 pb-1">
             Inputs – Unique
           </h4>
