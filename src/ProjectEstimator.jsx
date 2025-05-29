9469ece3870daa0c73d2ae33b081503f4b489d0a
alanbrennan1 <alanbrennan1@gmail.com>
2025-05-29 13:28:00 +0100

    Update ProjectEstimator.jsx

--- src/ProjectEstimator.jsx
+++ src/ProjectEstimator.jsx
@@ -1095,22 +1095,18 @@ setSelectedProduct('CT');  // Auto-return to CT tab
     
    {/* 🔢 Opportunity Number */}
 <div className="flex flex-col">
-  <label className="text-xs font-medium text-gray-700 mb-1">
-    Opportunity Number
-  </label>
+<label className="text-xs font-medium mb-1">Opportunity Number</label>
   <input
-    type="text"
     name="opportunityNumber"
     value={formData.opportunityNumber || ""}
     onChange={handleChange}
-    className="border border-gray-300 rounded px-3 py-2 text-sm"
+    className="border p-2 rounded text-xs"
     placeholder="Enter Opportunity Number"
   />
 </div>
   
    
-   
-   {/* Project Name */}
+      {/* Project Name */}
     <div className="flex flex-col">
       <label className="text-xs font-medium mb-1">Project Name</label>
       <input
