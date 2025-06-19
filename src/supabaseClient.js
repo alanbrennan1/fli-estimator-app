94d4cbdc5dbee90ee431b78a4d8aace97a2f95cc
alanbrennan1 <alanbrennan1@gmail.com>
2025-06-19 19:11:35 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,4 +7,10 @@ console.log('👀 SUPABASE_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  global: {
+    headers: {
+      Accept: 'application/json', // ✅ required to avoid 406 errors
+    },
+  },
+});
